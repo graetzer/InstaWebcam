a8eed50cc1c4db42ac5f252750e80584f8ff2fae
Simon Grätzer <simon@graetzer.org>
2013-09-16 14:23:26 +0200

    Initial Commit

---
+++ InstantWebcam/SGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SGAppDelegate.h
+//  InstantWebcam
+//
+//  Created by Simon Grätzer on 16.09.13.
+//  Copyright (c) 2013 Simon Grätzer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ InstantWebcam/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  InstantWebcam
+//
+//  Created by Simon Grätzer on 16.09.13.
+//  Copyright (c) 2013 Simon Grätzer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SGAppDelegate class]));
+    }
+}
